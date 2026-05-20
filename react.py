50119552c1c37fff77e1e7681d0dc39a0dadf92f
Gianluca Lambruschini <gianluca.lambruschini@gmail.com>
2026-05-20 14:37:33 -0300

    archivo de llamado de funciones

---
+++ react.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+from langchain_core.tools import tool
+from langchain_ollama import ChatOllama
+from langchain_tavily import TavilySearch
+
+load_dotenv()
+
+@tool
+def triple(num:float) -> float:
+    """
+    param num: a number to triple
+    returns: the triple of the input number
+    """
+    return float(num) * 3
+
+tools = [TavilySearch(max_results=1), triple]
+
+llm = ChatOllama(model="qwen3:8b",
+                api_key=os.getenv("OLLAMA_API_KEY"),
+                temperature=0).bind_tools(tools)
